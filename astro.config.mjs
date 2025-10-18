dcd3e1766bd60fd4d0ecc97197079c5105c93809
LinHeMa <109965534+LinHeMa@users.noreply.github.com>
2025-10-19 00:03:29 +0800

    chore: add astro/vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bubuchacha-git-master-linhemas-projects.vercel.app",
   integrations: [mdx(), sitemap()],
+  adapter: vercel(),
 });
